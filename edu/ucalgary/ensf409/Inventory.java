e62ec1f46778146641026e8d0765e898a641ce60
kirtan.kakadiya <kirtan.kakadiya@gmail.com>
2022-04-09 23:27:51 -0600

    SQL connects to ArrayList

--- edu/ucalgary/ensf409/Inventory.java
+++ edu/ucalgary/ensf409/Inventory.java
@@ -13,9 +13,9 @@ import java.sql.*;
 import java.util.*;
 
 public class Inventory {
-    private Food foodItem;
+    private static Food foodItem;
     private String foodQuantity = "";
-    private ArrayList<Inventory> items = new ArrayList<>();
+    private static ArrayList<Inventory> items = new ArrayList<>();
     private Nutrition nutritionalFacts;
     private Connection dbConnect;
     private ResultSet results;
@@ -24,7 +24,7 @@ public class Inventory {
         this.foodItem = foodItem;
         //this.foodQuantity = foodQuantity;
     }
-    
+    public Inventory(){}
     public void insertItem(Food item) throws IllegalArgumentException{
         Inventory inventoryItem = new Inventory(item);
         items.add(inventoryItem);
@@ -32,41 +32,81 @@ public class Inventory {
 
     public void initializeConnection() throws SQLException {
         try{
-            dbConnect = DriverManager.getConnection("jdbc:mysql://localhost/inventory", "student", "ensf409");
+            dbConnect = DriverManager.getConnection("jdbc:mysql://localhost/food_inventory", "root", "ensf409");
+            System.out.println("connection succeful");
         } catch (SQLException e){
             e.printStackTrace();
             throw new SQLException("Cannot connect to the database!");
         }
     }
 
-    public void getDataFromAvailableFood() throws SQLException{
+    public String getDataFromAvailableFood() throws SQLException{
+        StringBuilder output = new StringBuilder(300);
         try {
             Statement myStatement = dbConnect.createStatement();
-            
-            results = myStatement.executeQuery("SELECT ItemID,Name,GrainContent,FVContent,ProContent,Other,Calories FROM AVAILABLE_FOOD");
+            //System.out.println("markes");
+            results = myStatement.executeQuery("SELECT * FROM available_food");
+
             while(results.next()){
                 int wholeGrain = Integer.parseInt(results.getString("GrainContent"));
+
                 int fruitVeggie = Integer.parseInt(results.getString("FVContent"));
-                int protein = Integer.parseInt(results.getString("PVContent"));
+                int protein = Integer.parseInt(results.getString("ProContent"));
                 int other = Integer.parseInt(results.getString("Other"));
                 int calories = Integer.parseInt(results.getString("Calories"));
+                System.out.println( results.getString("ItemID") + " " + results.getString("Name") + " " + fruitVeggie + " "+ protein +" "+other + " " +calories);
 
                 nutritionalFacts = new Nutrition(wholeGrain,fruitVeggie,protein,other,calories);
                 foodItem = new Food(results.getString("ItemID"), results.getString("Name"), nutritionalFacts);
                 insertItem(foodItem);
                 //System.out.println(results.getString("Name") + " " + results.getString("GrainContent") + " " + results.getString("FVContent") + " " + results.getString("ProContent") + " " + results.getString("Other") + " ");
-
+                
             }
             myStatement.close();
         
         } catch (Exception e) {
+            System.out.println("failed");
             //TODO: Need to make it so it print an error message in the GUI but the program doesnt terminate
     }
+    return output.toString().trim();
+
+    }
+
+    public void deleteItemFromAvailableFood(String itemName) throws SQLException{
+        try{
+            String query = "DELETE FROM available_food WHERE Name = (?)";
+            PreparedStatement myStmt = dbConnect.prepareStatement(query);
+            myStmt.setString(1, itemName);
+
+            int rowCount = myStmt.executeUpdate();
+            if(rowCount!= 1){
+                throw new SQLException();
+            }
+            myStmt.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new SQLException();
+            //we need to handle if deletion doesnt work
+        }
+
+    }
+
+
+    
+    public static void main(String[] args) throws SQLException {
+        Inventory myJDBC = new Inventory();
+        myJDBC.initializeConnection();
+
+        
+        System.out.println(myJDBC.getDataFromAvailableFood());
 
 
     }
+
+    
 }
-}
+
 
 
 
